c5c0ff153cbffeddd612d80fabcbecbb3bd02ca1
Etienne Belde <ejm.belde@student.avans.nl>
2017-06-18 13:21:18 +0200

    server

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-//
-// server.js
-//
-var http = require('http');
 var express = require('express');
-var routes_v12 = require('./api/routes_v1');
-var routes_v2 = require('./api/routes_v2');
+var router = express.Router();
 
-var app = express();
-// api v1 en v2
-app.use('/api/v1', routes_v12);
-app.use('/api/v2', routes_v2);
+var auth = require('../auth/authentication');
 
-// Server hostnaam en poort
-app.listen(process.env.PORT || 3000, function(){
-	console.log('De server luistert op port 3000');	
+
+router.post('/login', function(req, res) {
+
+
+
+    // De username en password worden meegestuurd in de request body
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // Dit is een dummy-user die uit de database komt
+    // Momenteel zetten we ze als environment variabelen. 
+    var _dummy_username = process.env.APP_USERNAME || "username";
+    var _dummy_password = process.env.APP_PASSWORD || "test";
+
+    // Kijk of de gegevens matchen. Zo ja, dan token genereren en terugsturen.
+    if (username == _dummy_username && password == _dummy_password) {
+        var token = auth.encodeToken(username);
+        res.status(200).json({
+            "token": token,
+        });
+    } else {
+        // console.log('Input: username = ' + username + ', password = ' + password);
+        res.status(401).json({ "error": "Invalid credentials, bye" })
+    }
+
 });
 
-module.exports = app;
+// Zo wordt de route zichtbaar
+module.exports = router;
